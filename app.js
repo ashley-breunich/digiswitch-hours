d50478a06c286d158ca022e58ac1fa056936d609
Ashley Breunich <ashleybreunich@gmail.com>
2018-08-21 17:28:03 -0700

    Working on jQuery

--- app.js
+++ app.js
@@ -2,8 +2,17 @@
 
 let total = 0;
 
-$("#questions").hide();
-$("#begin").on("click", function() {
-    $("#begin").hide();
-    $("#questions").show();
+let serviceData = [
+    {
+        question: 'Are you interested in having our team manage your social media?',
+        answers: ['yes', 'no'],
+    }
+]
+
+$('#begin').on('click', function() {
+    $('#begin').hide();
+    $('#service-question').text(serviceData[0].question);
+    jQuery.each(serviceData[0].answers, function(index, value) {
+        $('#service-answers').add("li").text(value);
+    })
 })
